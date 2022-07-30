424c498e664dacb4c19c242405bf5aa684b5fa84
Denis <demaz@bk.ru>
2022-07-30 16:39:42 +0700

    task 3 done

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,10 @@ public class Main {
         //task2
 
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //task3
+
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё", "е").replace("Ё", "Е"));
     }
 }
