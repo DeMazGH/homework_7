a9a7f749467850ef5b34d76f8abce96918afa5a8
Denis <demaz@bk.ru>
2022-07-30 16:36:17 +0700

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,9 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.printf("ФИО сотрудника — %s\n", fullName);
+
+        //task2
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
 }
