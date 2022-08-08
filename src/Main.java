d87f4f6a4c10c4e6b73b0575b213eca05b7aa185
Denis <demaz@bk.ru>
2022-08-08 10:42:59 +0700

    homework_7 fix

--- src/Main.java
+++ src/Main.java
@@ -43,7 +43,6 @@ public class Main {
         fullName = "ivanov ivan ivanovich";
         char[] name = fullName.toCharArray();
         name[0] = Character.toUpperCase(name[0]);
-
         name[fullName.indexOf(' ') + 1] = Character.toUpperCase(name[fullName.indexOf(' ') + 1]);
         name[fullName.lastIndexOf(' ') + 1] = Character.toUpperCase(name[fullName.lastIndexOf(' ') + 1]);
 
